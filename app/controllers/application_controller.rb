5f934e80ecfe34798a87c3bb96869d8145b5b0f6
yurippe7575 <69247926+yurippe7575@users.noreply.github.com>
2020-09-11 08:09:18 +0900

    新規会員登録可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!,except: [:index]
+  #before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -8,4 +8,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:last_name,:family_name_kana,:last_name_kana,:birth_day])
   end
+
 end
