e881c681fee7a0206110e10b742c73db16273015
yurippe7575 <69247926+yurippe7575@users.noreply.github.com>
2020-09-17 15:00:44 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -65,3 +65,7 @@ gem 'devise'
 gem 'active_hash'
 
 gem 'gimei'
+
+gem 'mini_magick'
+
+gem 'image_processing', '~> 1.2'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  has_one_attached :image
 
+
+  private
   
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+
 end
