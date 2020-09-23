3ced3d2ca2e98737a3ed45dc074e7faac77a514b
yurippe7575 <69247926+yurippe7575@users.noreply.github.com>
2020-09-23 23:51:13 +0900

    削除の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -36,13 +36,13 @@ class ItemsController < ApplicationController
     end
   end
 
-    def destroy
-      if @item.destroy
-        redirect_to root_path
-      else
-        render :show
-      end
-   end
+  #   def destroy
+  #     if @item.destroy
+  #       redirect_to root_path
+  #     else
+  #       render :show
+  #     end
+  #  end
 
 
 
